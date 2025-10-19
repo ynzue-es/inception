0467369b1cbe6d05f4c8f25e812d4b4530d434c6
yannis <yannis.nzuepro@gmail.com>
2025-10-19 17:52:23 +0200

    remove inofs

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 service mariadb start
 
+DB_PASS=$(cat /run/secrets/db_password)
+
 echo "CREATE DATABASE IF NOT EXISTS wordpress;" > /tmp/init.sql
-echo "CREATE USER IF NOT EXISTS 'wp_user'@'%' IDENTIFIED BY 'doerg!eKUU073@iergzrIUG';" >> /tmp/init.sql
+echo "CREATE USER IF NOT EXISTS 'wp_user'@'%' IDENTIFIED BY '${DB_PASS}';" >> /tmp/init.sql
 echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'wp_user'@'%';" >> /tmp/init.sql
 echo "FLUSH PRIVILEGES;" >> /tmp/init.sql
 
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 service php8.2-fpm start
-sleep 5
+
+DB_PASS=$(cat /run/secrets/db_password)
+DB_ROOT_PASS=$(cat /run/secrets/db_root_password)
+
+DB_HOST=${WORDPRESS_DB_HOST}
+DB_USER=${WORDPRESS_DB_USER}
+DB_NAME=${WORDPRESS_DB_NAME}
+SITE_URL=${WORDPRESS_URL}
+SITE_TITLE=${WORDPRESS_TITLE}
+ADMIN_USER=${WORDPRESS_ADMIN_USER}
+ADMIN_EMAIL=${WORDPRESS_ADMIN_EMAIL}
+
 wp core download --allow-root --path=/var/www/html
 wp config create --allow-root \
-  --dbname=wordpress \
-  --dbuser=wp_user \
-  --dbpass=doerg!eKUU073@iergzrIUG \
-  --dbhost=mariadb:3306
+--dbname=${DB_NAME} \
+--dbuser=${DB_USER} \
+--dbpass=${DB_PASS} \
+--dbhost=${DB_HOST}
+
 wp core install --allow-root \
-  --url=https://ynzue-es.42.fr \
-  --title="Inception" \
-  --admin_user=yannis \
-  --admin_password=siudYIUTugu976!!erfz@zseOIY \
-  --admin_email=ynzue-es@student.42lyon.fr
-php-fpm8.2 -F
+--url=${SITE_URL} \
+--title="${SITE_TITLE}" \
+--admin_user=${ADMIN_USER} \
+--admin_password=${DB_ROOT_PASS} \
+--admin_email=${ADMIN_EMAIL}
+
+exec php-fpm8.2 -F
